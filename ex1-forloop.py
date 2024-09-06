3e1e2ad0780edd972fc428436eb1afbe0f21bffa
quannm0309 <quannm.0309@gmail.com>
2024-09-07 06:41:26 +0900

    Update ex1-forloop.py 2nd line from techteam1 on Github

--- ex1-forloop.py
+++ ex1-forloop.py
@@ -3,4 +3,6 @@ print(" The Program ran successfully")
 print("Added this line of code from git Terminal")
 #----------------------------------------------------------
 print("Added this line of code from techteam1 on GitHub")
+#----------------------------------------------------------
+print("Added this 2nd line of code from techteam1 on GitHub")
 
