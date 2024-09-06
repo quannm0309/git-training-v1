71751ce320f6ed7ed8c97095b0f66a7d2dd652d4
quannm0309 <quannm.0309@gmail.com>
2024-09-07 06:39:51 +0900

    Update ex1-forloop.py from techteam1 on GitHub

--- ex1-forloop.py
+++ ex1-forloop.py
@@ -1,4 +1,6 @@
 print(" The Program ran successfully")
 #----------------------------------------------------------
 print("Added this line of code from git Terminal")
+#----------------------------------------------------------
+print("Added this line of code from techteam1 on GitHub")
 
